aa09a9d123a387d849957052e7ba71362055ad61
uru-anikira <ammygirl92@gmail.com>
2026-02-09 18:08:34 -0500

    Create PlantMasterlist1.0.js

---
+++ PlantMasterlist1.0.js
@@ -0,0 +1,65 @@
+const plants [
+    {
+        id: "aghada",
+        name: "Aghada Leaf",
+        scientific: "Achyranthens Aspera",
+        foundIn: ["Irengu Plains", "Rynack"],
+        description: "Grows in shade of trees, inverted cone with pale yellow bristled flower",
+        seasons: ["Spring, Summer"],
+        skill: ["2"],
+        potency: ["1"],
+        beneficial: ["Flower"],
+        poisonous: [],
+                  
+        medicinal: ["Ingestion"],
+        treats: ["Cough","Fever","Pain Relief"],
+      
+        toxic: [],
+        complications: [],
+
+        native: ["No"],
+        uniqueuse: []
+      },
+      {
+        id: "agrimony",
+        name: "Agrimony",
+        scientific: "Agrimonia Eupatoria",
+        foundIn: ["Irengu Plains", "Rynack"],
+        description: "Grows in shaded areas, clustered plant with five petal yellow flowers",
+        seasons: ["Spring, Summer"],
+        skill: ["3"],
+        potency: ["1"],
+        beneficial: ["Flower"],
+        poisonous: [],
+                  
+        medicinal: ["Topical"],
+        treats: ["Pain","Accelerate Healing"],
+      
+        toxic: ["Ingestion"],
+        complications: ["Nausea","Death"],
+
+        native: ["No"],
+        uniqueuse: []
+      },
+      {
+        id: "angelica",
+        name: "Angelica",
+        scientific: "Angelica Archangelica",
+        foundIn: ["Irengu Plains", "Rynack"],
+        description: "Grows in shaded areas, clustered plant with five petal yellow flowers",
+        seasons: ["Spring, Summer"],
+        skill: ["3"],
+        potency: ["1"],
+        beneficial: ["Flower"],
+        poisonous: [],
+                  
+        medicinal: ["Topical"],
+        treats: ["Pain","Accelerate Healing"],
+      
+        toxic: ["Ingestion"],
+        complications: ["Nausea","Death"],
+
+        native: ["No"],
+        uniqueuse: []
+      }
+];
