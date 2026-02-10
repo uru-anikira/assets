55c9eec0afeb84e2159769569c0cd6eaf55f4e85
uru-anikira <ammygirl92@gmail.com>
2026-02-10 18:15:02 -0500

    Update ParentBoardMapping1.2.js

--- ParentBoardMapping1.2.js
+++ ParentBoardMapping1.2.js
@@ -103,9 +103,10 @@ const parentBoardMapping = [
   {
     parentId: "21",
     parentName: "Vor'asa",
-    subregions: ["The South Rim","The North Rim","Dalv Glad","Eel's Tail River","Lod'nen Path","Adeli's Cove","SS Rilon","The Waste","Layrun Trench","Angitu Desert","Orvor Rock","West Delia Basin","North Delia Basin]
+    subregions: ["The South Rim","The North Rim","Dalv Glad","Eel's Tail River","Lod'nen Path","Adeli's Cove","SS Rilon","The Waste","Layrun Trench","Angitu Desert","Orvor Rock","West Delia Basin","North Delia Basin"]
   }
   // Add other parent regions as needed
 ];
+
 
 
